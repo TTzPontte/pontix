5b310abbe1471887976df7f00439e8c8edce64e2
Hugo <hugo@pontte.com.br>
2021-09-25 18:41:58 -0300

    first commit

---
+++ makes.nix
@@ -0,0 +1,26 @@
+{ fetchNixpkgs
+, outputs
+, ...
+}: {
+  extendingMakesDir = "/nix";
+  projectIdentifier = "pontix";
+  formatNix = {
+    enable = true;
+    targets = [ "/" ];
+  };
+  inputs = rec {
+    sources = import nix/sources.nix;
+    nixpkgs = import sources.nixpkgs { overlays = [] ; config = {}; };
+  };
+  lintGitCommitMsg = {
+    enable = true;
+    branch = "main";
+  };
+  secretsForAwsFromEnv = {
+    makesProd = {
+      accessKeyId = "AWS_ACCESS_KEY_ID";
+      secretAccessKey = "AWS_SECRET_ACCESS_KEY";
+      defaultRegion = "AWS_DEFAULT_REGION";
+    };
+  };
+}
