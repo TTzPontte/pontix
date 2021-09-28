017d81f5e64047f35dbae38cda505d12b2953a75
Hugo <hugo@pontte.com.br>
2021-09-28 04:23:59 -0300

    chore(build): cache

--- nix/gh-actions.nix
+++ nix/gh-actions.nix
@@ -48,7 +48,7 @@ rec {
     args@{ name ? "build"
     , steps ? [ ]
     , stepsBefore ? [ installNix installMakes checkout ]
-    , stepsAfter ? [ notifyStep pushCache ]
+    , stepsAfter ? [ pushCache notifyStep ]
     , ...
     }: {
       ${name} = (builtins.removeAttrs args [ "name" ]) // {
--- nix/pushCache/main.nix
+++ nix/pushCache/main.nix
@@ -4,14 +4,16 @@
 , ...
 }:
 makeScript {
-  name = "pushCache";
+  name = "push-cache";
   searchPaths = {
     bin = [
       inputs.nixpkgs.nix
     ];
   };
   entrypoint = ''
-    nix sign-path --key-file <(echo "$PONTIX_PRIV_KEY") --all
+    ramtmp="$(mktemp -p /dev/shm/)"
+    echo "$PONTIX_PRIV_KEY" > $ramtmp
+    nix sign-paths --key-file $ramtmp --all
     nix copy --to s3://pontte-nix-cache --all
   '';
 }
