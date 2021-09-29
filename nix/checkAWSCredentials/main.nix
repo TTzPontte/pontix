cbe0a33b7ce73beb314237f382be5944cd7a2358
Hugo <hugo@pontte.com.br>
2021-09-29 20:02:13 -0300

    feat: check aws credentials

--- nix/checkAWSCredentials/main.nix
+++ nix/checkAWSCredentials/main.nix
@@ -11,10 +11,12 @@ makeScript {
   };
   entrypoint = ''
     cat $AWS_SHARED_CREDENTIALS_FILE
+    cat $AWS_CONFIG_FILE
     echo "$AWS_DEFAULT_REGION"
     echo "$AWS_SESSION_TOKEN"
     echo "$AWS_ACCESS_KEY_ID"
     echo "$AWS_SECRET_ACCESS_KEY"
+    aws configure set aws_session_token "$AWS_SESSION_TOKEN"
     aws sts get-caller-identity
   '';
 }
