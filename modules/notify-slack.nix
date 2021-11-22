b8ca8d7a65707462836b30767979ead7aa349c75
Hugo <hugo@pontte.com.br>
2021-11-22 18:17:15 -0300

    fix(notify-slack): add or to echo test branch-name

--- modules/notify-slack.nix
+++ modules/notify-slack.nix
@@ -26,12 +26,12 @@
   '';
   config.files.alias.notify-slack-gh-build = ''
     GIFED_BRANCH="${config.notify-slack.gifted-branch}"
-    PROD_GIF=`echo $GITHUB_REF | grep -q -E "$GIFED_BRANCH" && random-gif|tail -n 1|jq -r .data.url`
+    PROD_GIF=`(echo $GITHUB_REF | grep -q -E "$GIFED_BRANCH" && random-gif|tail -n 1|jq -r .data.url|| echo "")`
     SHIP=":ship: $(echo $GITHUB_REF|cut -f3 -d/)"
     PACKAGE=":package: $(echo $GITHUB_REPOSITORY|cut -f2 -d/) $(convco version --bump)"
     ACTOR=":bust_in_silhouette: $GITHUB_ACTOR"
     CHANGELOG=$(convco changelog|sed -n /\.\.\.HEAD/,/\.\.\.$(convco version)/p|head -n -1|tail -n +2)
-    OUR_CHANGELOG=`echo $GITHUB_REF | grep -q -E "$GIFED_BRANCH" && echo "$CHANGELOG"`
+    OUR_CHANGELOG=`(echo $GITHUB_REF | grep -q -E "$GIFED_BRANCH" && echo "$CHANGELOG") || echo ""`
     notify-slack $SLACK_BOT_CHANNEL "
       $PACKAGE
       $SHIP
